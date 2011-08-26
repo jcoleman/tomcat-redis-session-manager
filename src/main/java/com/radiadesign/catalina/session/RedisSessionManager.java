57c859422845cbec918b70ee6b68dbaf60828439
James Coleman <jtc331@gmail.com>
2011-08-26 11:25:16 -0400

    Override other necessary parts of the ManagerBase implementation (such as add(session) and createSession()) since the default implementation relies on local session storage. In this we also ensure that session identifiers are unique in Redis. Update manager to use the updated dirty tracking of RedisSession. Capture race condition where session identifier has been created but not yet persisted and throw an intelligent error. Only load session from Redis if we haven't already tried once for that identifier in the current request in the current thread.

--- src/main/java/com/radiadesign/catalina/session/RedisSessionManager.java
+++ src/main/java/com/radiadesign/catalina/session/RedisSessionManager.java
@@ -16,6 +16,7 @@ import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -24,6 +25,9 @@ import java.util.logging.Logger;
 
 
 public class RedisSessionManager extends ManagerBase implements Lifecycle {
+
+  protected byte[] NULL_SESSION = "null".getBytes();
+
   private static Logger log = Logger.getLogger("RedisSessionManager");
   protected String host = "localhost";
   protected int port = 6379;
@@ -32,7 +36,11 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
 
   protected RedisSessionHandlerValve handlerValve;
   protected ThreadLocal<RedisSession> currentSession = new ThreadLocal<RedisSession>();
+  protected ThreadLocal<String> currentSessionId = new ThreadLocal<String>();
+  protected ThreadLocal<Boolean> currentSessionIsPersisted = new ThreadLocal<Boolean>();
   protected Serializer serializer;
+
+  protected static String name = "RedisSessionManager";
 
   protected String serializationStrategyClass = "com.radiadesign.catalina.session.JavaSerializer";
   
@@ -87,7 +95,7 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
 
     return jedis;
   }
-  
+
   protected void returnConnection(Jedis jedis, Boolean error) {
     if (error) {
       connectionPool.returnBrokenResource(jedis);
@@ -117,7 +125,6 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
     lifecycle.addLifecycleListener(listener);
   }
 
-
   /**
    * Get the lifecycle listeners associated with this lifecycle. If this
    * Lifecycle has no listeners registered, a zero-length array is returned.
@@ -134,11 +141,6 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
    */
   public void removeLifecycleListener(LifecycleListener listener) {
     lifecycle.removeLifecycleListener(listener);
-  }
-
-  @Override
-  public Session createEmptySession() {
-    return new RedisSession(this);
   }
 
   public void start() throws LifecycleException {
@@ -169,7 +171,7 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
     initializeDatabaseConnection();
 
     setDistributable(true);
-    
+
     lifecycle.fireLifecycleEvent(START_EVENT, null);
   }
 
@@ -183,8 +185,89 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
     lifecycle.fireLifecycleEvent(STOP_EVENT, null);
   }
 
+  @Override
+  public Session createSession() {
+    RedisSession session = (RedisSession)createEmptySession();
+
+    // Initialize the properties of the new session and return it
+    session.setNew(true);
+    session.setValid(true);
+    session.setCreationTime(System.currentTimeMillis());
+    session.setMaxInactiveInterval(getMaxInactiveInterval());
+
+    String sessionId;
+    String jvmRoute = getJvmRoute();
+
+    Boolean error = true;
+    Jedis jedis = null;
+
+    try {
+      jedis = acquireConnection();
+
+      // Ensure generation of a unique session identifier.
+      do {
+        sessionId = generateSessionId();
+
+        if (jvmRoute != null) {
+          sessionId += '.' + jvmRoute;
+        }
+      } while (jedis.setnx(sessionId.getBytes(), NULL_SESSION) == 1L); // 1 = key set; 0 = key already existed
+
+      /* Even though the key is set in Redis, we are not going to flag
+         the current thread as having had the session persisted since
+         the session isn't actually serialized to Redis yet.
+         This ensures that the save(session) at the end of the request
+         will serialize the session into Redis with 'set' instead of 'setnx'. */
+
+      error = false;
+
+      session.setId(sessionId);
+      session.tellNew();
+    } finally {
+      if (jedis != null) {
+        returnConnection(jedis, error);
+      }
+    }
+
+    return session;
+  }
+
+  @Override
+  public Session createEmptySession() {
+    return new RedisSession(this);
+  }
+
+  @Override
+  public void add(Session session) {
+    try {
+      save(session);
+    } catch (IOException ex) {
+      log.warning("Unable to add to session manager store: " + ex.getMessage());
+      throw new RuntimeException("Unable to add to session manager store.", ex);
+    }
+  }
+
+  @Override
   public Session findSession(String id) throws IOException {
-    return loadSession(id);
+    RedisSession session;
+
+    if (id == null) {
+      session = null;
+      currentSessionIsPersisted.set(false);
+    } else if (id.equals(currentSessionId.get())) {
+      session = currentSession.get();
+    } else {
+      session = loadSessionFromRedis(id);
+      
+      if (session != null) {
+        currentSessionIsPersisted.set(true);
+      }
+    }
+
+    currentSession.set(session);
+    currentSessionId.set(id);
+
+    return session;
   }
 
   public void clear() {
@@ -231,17 +314,8 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
     }
   }
 
-  public Session loadSession(String id) throws IOException {
+  public RedisSession loadSessionFromRedis(String id) throws IOException {
     RedisSession session;
-    
-    session = currentSession.get();
-    if (session != null) {
-      if (id.equals(session.getId())) {
-        return session;
-      } else {
-        currentSession.remove();
-      }
-    }
 
     Jedis jedis = null;
     Boolean error = true;
@@ -256,8 +330,10 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
       if (data == null) {
         log.fine("Session " + id + " not found in Redis");
         session = null;
+      } else if (Arrays.equals(NULL_SESSION, data)) {
+        throw new IllegalStateException("Race condition encountered: attempted to load session[" + id + "] which has been created but not yet serialized.");
       } else {
-        log.fine("Deserializing session from Redis");
+        log.fine("Deserializing session " + id + " from Redis");
         session = (RedisSession)createEmptySession();
         serializer.deserializeInto(data, session);
         session.setId(id);
@@ -265,25 +341,22 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
         session.setMaxInactiveInterval(getMaxInactiveInterval() * 1000);
         session.access();
         session.setValid(true);
-        session.resetChangedAttributes();
+        session.resetDirtyTracking();
 
         if (log.isLoggable(Level.FINE)) {
-          log.fine("Session Contents [" + session.getId() + "]:");
+          log.fine("Session Contents [" + id + "]:");
           for (Object name : Collections.list(session.getAttributeNames())) {
               log.fine("  " + name);
           }
         }
-
-        log.fine("Loaded session id " + id);
       }
 
-      currentSession.set(session);
       return session;
     } catch (IOException e) {
       log.severe(e.getMessage());
       throw e;
     } catch (ClassNotFoundException ex) {
-      log.log(Level.SEVERE, "Unable to deserialize into session ", ex);
+      log.log(Level.SEVERE, "Unable to deserialize into session", ex);
       throw new IOException("Unable to deserialize into session", ex);
     } finally {
       if (jedis != null) {
@@ -310,19 +383,19 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
 
       Boolean sessionIsDirty = redisSession.isDirty();
 
-      redisSession.resetChangedAttributes();
+      redisSession.resetDirtyTracking();
       byte[] binaryId = redisSession.getId().getBytes();
       byte[] data = serializer.serializeFrom(redisSession);
 
       jedis = acquireConnection();
 
-      if (sessionIsDirty) {
+      if (sessionIsDirty || currentSessionIsPersisted.get() != true) {
         jedis.set(binaryId, data);
       } else {
-        // TODO: Only do this if this session object was not loaded from the database.
-        //       Or if forced (potentially on first save).
         jedis.setnx(binaryId, data);
       }
+
+      currentSessionIsPersisted.set(true);
 
       log.fine("Setting expire timeout on session [" + redisSession.getId() + "] to " + getMaxInactiveInterval());
       jedis.expire(binaryId, getMaxInactiveInterval());
@@ -342,8 +415,9 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
   public void remove(Session session) {
     Jedis jedis = null;
     Boolean error = true;
-    
+
     log.fine("Removing session ID : " + session.getId());
+
     try {
       jedis = acquireConnection();
       jedis.del(session.getId());
@@ -359,10 +433,13 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
     RedisSession redisSession = currentSession.get();
     if (redisSession != null) {
       currentSession.remove();
+      currentSessionId.remove();
+      currentSessionIsPersisted.remove();
       log.fine("Session removed from ThreadLocal :" + redisSession.getIdInternal());
     }
   }
 
+  @Override
   public void processExpires() {
     // We are going to use Redis's ability to expire keys for session expiration.
 
