fdde9117cfbaef55fabcf76ef9c6c47ed237bdf0
James Coleman <jtc331@gmail.com>
2012-09-26 16:45:49 -0300

    Fix configuration methods missing "static".

--- src/main/java/com/radiadesign/catalina/session/RedisSession.java
+++ src/main/java/com/radiadesign/catalina/session/RedisSession.java
@@ -14,13 +14,13 @@ public class RedisSession extends StandardSession {
 
   protected static Boolean manualDirtyTrackingSupportEnabled = false;
 
-  public void setManualDirtyTrackingSupportEnabled(Boolean enabled) {
+  public static void setManualDirtyTrackingSupportEnabled(Boolean enabled) {
     manualDirtyTrackingSupportEnabled = enabled;
   }
 
   protected static String manualDirtyTrackingAttributeKey = "__changed__";
 
-  public void setManualDirtyTrackingAttributeKey(String key) {
+  public static void setManualDirtyTrackingAttributeKey(String key) {
     manualDirtyTrackingAttributeKey = key;
   }
 
