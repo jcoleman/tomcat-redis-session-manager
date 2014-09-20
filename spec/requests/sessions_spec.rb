9d14c14c59dc1f5a99d60d550d633f88d7364911
James Coleman <jtc331@gmail.com>
2014-09-20 15:27:29 -0400

    Remove incorrectly committed commented out code.

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,33 +1,4 @@
 require 'spec_helper'
-#require "rails_helper"
-#require 'action_controller'
-#require 'action_view'
-#require 'active_support'
-#require 'active_support/core_ext'
-
-#require 'active_support/all'
-#require 'action_controller'
-#require 'action_dispatch'
-#
-#module Rails
-#  class App
-#    def env_config; {} end
-#    def routes
-#      return @routes if defined?(@routes)
-#      @routes = ActionDispatch::Routing::RouteSet.new
-#      @routes.draw do
-#        resources :posts # Replace with your own needs
-#      end
-#      @routes
-#    end
-#  end
-#
-#  def self.application
-#    @app ||= App.new
-#  end
-#end
-#
-#require 'rspec/rails'
 
 SESSION_PATH = '/session'
 SESSIONS_PATH = '/sessions'
