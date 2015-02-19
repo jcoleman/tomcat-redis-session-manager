e86c8c00d45f2c72ca74b806211f5ff558e42b8a
Michael Cameron <mcameron@gravie.com>
2015-02-19 10:13:00 -0600

    Do not wipe out current session when calling findSession and no session is found

--- src/main/java/com/orangefunction/tomcat/redissessions/RedisSessionManager.java
+++ src/main/java/com/orangefunction/tomcat/redissessions/RedisSessionManager.java
@@ -422,10 +422,7 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
     RedisSession session = null;
 
     if (null == id) {
-      currentSessionIsPersisted.set(false);
-      currentSession.set(null);
-      currentSessionSerializationMetadata.set(null);
-      currentSessionId.set(null);
+      return null;
     } else if (id.equals(currentSessionId.get())) {
       session = currentSession.get();
     } else {
@@ -437,11 +434,6 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
         currentSessionSerializationMetadata.set(container.metadata);
         currentSessionIsPersisted.set(true);
         currentSessionId.set(id);
-      } else {
-        currentSessionIsPersisted.set(false);
-        currentSession.set(null);
-        currentSessionSerializationMetadata.set(null);
-        currentSessionId.set(null);
       }
     }
 
