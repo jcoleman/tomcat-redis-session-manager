25b1b64ef1bc15b8647ea23b4feca2d827aa2409
ice <zbp1025@163.com>
2015-04-16 13:55:01 +0800

    format source and remove the eclipse warning

--- src/main/java/com/orangefunction/tomcat/redissessions/JavaSerializer.java
+++ src/main/java/com/orangefunction/tomcat/redissessions/JavaSerializer.java
@@ -2,7 +2,6 @@ package com.orangefunction.tomcat.redissessions;
 
 import org.apache.catalina.util.CustomObjectInputStream;
 
-import javax.servlet.http.HttpSession;
 
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -14,68 +13,70 @@ import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
 public class JavaSerializer implements Serializer {
-  private ClassLoader loader;
+	private ClassLoader loader;
 
-  private final Log log = LogFactory.getLog(JavaSerializer.class);
+	private final Log log = LogFactory.getLog(JavaSerializer.class);
 
-  @Override
-  public void setClassLoader(ClassLoader loader) {
-    this.loader = loader;
-  }
+	@Override
+	public void setClassLoader(ClassLoader loader) {
+		this.loader = loader;
+	}
 
-  public byte[] attributesHashFrom(RedisSession session) throws IOException {
-    HashMap<String,Object> attributes = new HashMap<String,Object>();
-    for (Enumeration<String> enumerator = session.getAttributeNames(); enumerator.hasMoreElements();) {
-      String key = enumerator.nextElement();
-      attributes.put(key, session.getAttribute(key));
-    }
+	public byte[] attributesHashFrom(RedisSession session) throws IOException {
+		HashMap<String, Object> attributes = new HashMap<String, Object>();
+		for (Enumeration<String> enumerator = session.getAttributeNames(); enumerator
+				.hasMoreElements();) {
+			String key = enumerator.nextElement();
+			attributes.put(key, session.getAttribute(key));
+		}
 
-    byte[] serialized = null;
+		byte[] serialized = null;
 
-    try (
-         ByteArrayOutputStream bos = new ByteArrayOutputStream();
-         ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(bos));
-    ) {
-      oos.writeUnshared(attributes);
-      oos.flush();
-      serialized = bos.toByteArray();
-    }
+		try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream(
+						new BufferedOutputStream(bos));) {
+			oos.writeUnshared(attributes);
+			oos.flush();
+			serialized = bos.toByteArray();
+		}
 
-    MessageDigest digester = null;
-    try {
-      digester = MessageDigest.getInstance("MD5");
-    } catch (NoSuchAlgorithmException e) {
-      log.error("Unable to get MessageDigest instance for MD5");
-    }
-    return digester.digest(serialized);
-  }
+		MessageDigest digester = null;
+		try {
+			digester = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			log.error("Unable to get MessageDigest instance for MD5");
+		}
+		return digester.digest(serialized);
+	}
 
-  @Override
-  public byte[] serializeFrom(RedisSession session, SessionSerializationMetadata metadata) throws IOException {
-    byte[] serialized = null;
+	@Override
+	public byte[] serializeFrom(RedisSession session,
+			SessionSerializationMetadata metadata) throws IOException {
+		byte[] serialized = null;
 
-    try (
-         ByteArrayOutputStream bos = new ByteArrayOutputStream();
-         ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(bos));
-    ) {
-      oos.writeObject(metadata);
-      session.writeObjectData(oos);
-      oos.flush();
-      serialized = bos.toByteArray();
-    }
+		try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream(
+						new BufferedOutputStream(bos));) {
+			oos.writeObject(metadata);
+			session.writeObjectData(oos);
+			oos.flush();
+			serialized = bos.toByteArray();
+		}
 
-    return serialized;
-  }
+		return serialized;
+	}
 
-  @Override
-  public void deserializeInto(byte[] data, RedisSession session, SessionSerializationMetadata metadata) throws IOException, ClassNotFoundException {
-    try(
-        BufferedInputStream bis = new BufferedInputStream(new ByteArrayInputStream(data));
-        ObjectInputStream ois = new CustomObjectInputStream(bis, loader);
-    ) {
-      SessionSerializationMetadata serializedMetadata = (SessionSerializationMetadata)ois.readObject();
-      metadata.copyFieldsFrom(serializedMetadata);
-      session.readObjectData(ois);
-    }
-  }
+	@Override
+	public void deserializeInto(byte[] data, RedisSession session,
+			SessionSerializationMetadata metadata) throws IOException,
+			ClassNotFoundException {
+		try (BufferedInputStream bis = new BufferedInputStream(
+				new ByteArrayInputStream(data));
+				ObjectInputStream ois = new CustomObjectInputStream(bis, loader);) {
+			SessionSerializationMetadata serializedMetadata = (SessionSerializationMetadata) ois
+					.readObject();
+			metadata.copyFieldsFrom(serializedMetadata);
+			session.readObjectData(ois);
+		}
+	}
 }
--- src/main/java/com/orangefunction/tomcat/redissessions/RedisSessionHandlerValve.java
+++ src/main/java/com/orangefunction/tomcat/redissessions/RedisSessionHandlerValve.java
@@ -1,6 +1,5 @@
 package com.orangefunction.tomcat.redissessions;
 
-import org.apache.catalina.Session;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
@@ -11,21 +10,21 @@ import java.io.IOException;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
-
 public class RedisSessionHandlerValve extends ValveBase {
-  private final Log log = LogFactory.getLog(RedisSessionManager.class);
-  private RedisSessionManager manager;
+	private final Log log = LogFactory.getLog(RedisSessionManager.class);
+	private RedisSessionManager manager;
 
-  public void setRedisSessionManager(RedisSessionManager manager) {
-    this.manager = manager;
-  }
+	public void setRedisSessionManager(RedisSessionManager manager) {
+		this.manager = manager;
+	}
 
-  @Override
-  public void invoke(Request request, Response response) throws IOException, ServletException {
-    try {
-      getNext().invoke(request, response);
-    } finally {
-      manager.afterRequest();
-    }
-  }
+	@Override
+	public void invoke(Request request, Response response) throws IOException,
+			ServletException {
+		try {
+			getNext().invoke(request, response);
+		} finally {
+			manager.afterRequest();
+		}
+	}
 }
